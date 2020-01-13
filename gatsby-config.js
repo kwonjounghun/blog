9547c7f155f8da78c3aad48010fe8ec24ba02d60
jounghun.kwon <jounghun.kwon@wadiz.kr>
2020-01-13 19:57:54 +0900

    jsx templete 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    'gatsby-plugin-styled-jsx',
     'gatsby-plugin-netlify-cms',
     `gatsby-plugin-react-helmet`,
     {
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = props => {
+    return (
+        <header>
+            header
+        </header>
+    )
+}
+
+Header.propTypes = {
+
+}
+
+export default Header
