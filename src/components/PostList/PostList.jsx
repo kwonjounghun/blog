bc5321304f957ca7e33ba38abad0fb16e8e06e1c
jounghun.kwon <jounghun.kwon@wadiz.kr>
2020-03-29 16:56:01 +0900

    search fillter styling

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import PostCard from '../PostCard';
 import PostListHeader from '../PostListHeader';
+import PostListSearchFillter from '../PostListSearchFillter';
 
 import styles from './PostList.module.scss';
 
 const PostList = () => {
+	const [useFillter, setUseFillter] = useState(false);
+
 	return (
 		<div className={cx(styles.postList)}>
-			<PostListHeader />
+			<PostListHeader useFillter={useFillter} setUseFillter={setUseFillter} />
+			<PostListSearchFillter useFillter={useFillter} setUseFillter={setUseFillter} />
 			<PostCard />
 		</div>
 	)
