d4c783e22e4f6a200e6fb1c43574e2f9cd3bb263
jounghun.kwon <jounghun.kwon@wadiz.kr>
2020-04-01 01:02:12 +0900

    navigation submenu 구현

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const lodash = require('lodash');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 const getCollectionList = async (graphql) => {
-  const collections = await graphql(`
+	const collections = await graphql(`
     query loadPagesQuery {
       allMarkdownRemark {
         edges {
@@ -15,22 +17,22 @@ const getCollectionList = async (graphql) => {
       }
     }
   `).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+		if (result.errors) {
+			throw result.errors
+		}
 
-    // Create blog post pages.
-    const collections = lodash.map(result.data.allMarkdownRemark.edges, 'node.frontmatter.collection');
-    const uniqByCollections = lodash.uniqBy(collections);
+		// Create blog post pages.
+		const collections = lodash.map(result.data.allMarkdownRemark.edges, 'node.frontmatter.collection');
+		const uniqByCollections = lodash.uniqBy(collections);
 
-    return uniqByCollections;
-  });
+		return uniqByCollections;
+	});
 
-  return collections;
+	return collections;
 }
 
 const getOneCollectionList = async (graphql, collection) => {
-  const collections = await graphql(`
+	const collections = await graphql(`
     query loadPagesQuery {
       allMarkdownRemark(filter: {frontmatter: {private: {eq: true}, collection: {eq: "${collection}"}}}) {
         edges {
@@ -49,68 +51,88 @@ const getOneCollectionList = async (graphql, collection) => {
       }
     }
   `).then(result => {
-    if (result.errors) {
-      throw result.errors;
-    }
+		if (result.errors) {
+			throw result.errors;
+		}
 
-    return lodash.map(result.data.allMarkdownRemark.edges, 'node.frontmatter');
-  });
+		return lodash.map(result.data.allMarkdownRemark.edges, 'node.frontmatter');
+	});
 
-  return collections;
+	return collections;
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  // const indexPage = path.resolve('src/templates/main.jsx');
-  // Query for markdown nodes to use in creating pages.
-  // You can query for whatever data you want to create pages for e.g.
-  // products, portfolio items, landing pages, etc.
-  // Variables can be added as the second function parameter
+	const { createPage } = actions;
+	let configs = {}
 
-  const collections = await getCollectionList(graphql);
+	try {
+		configs = yaml.load(fs.readFileSync('./static/admin/config.yml', 'utf8'));
+	} catch (error) {
+		console.error(error);
+	}
+	// const indexPage = path.resolve('src/templates/main.jsx');
+	// Query for markdown nodes to use in creating pages.
+	// You can query for whatever data you want to create pages for e.g.
+	// products, portfolio items, landing pages, etc.
+	// Variables can be added as the second function parameter
 
-  const siteInfo = {
-    collections,
-    tags: [],
-  }
+	const collections = configs.collections.map(collection => {
+		let category = [];
+		lodash.forEach(collection.fields, (field) => {
+			if (collection.label !== 'Home' && field.name === 'category') {
+				category = field.options;
+			}
+		})
+		return {
+			name: collection.label,
+			discription: collection.discription,
+			category,
+		};
+	});
 
-  const createIndexPage = () => {
-    const Template = path.resolve(`src/templates/IndexPage.jsx`);
-    createPage({
-      path: '/',
-      component: Template,
-      // In your blog post template's graphql query, you can use path
-      // as a GraphQL variable to query for data from the markdown file.
-      context: {
-        siteInfo,
-      },
-    });
-  };
+	console.log('collections', collections);
 
-  const listPageData = collections.map(async (item) => {
-    const data = await getOneCollectionList(graphql, item);
-    console.log('data', data);
-    return data;
-  });
+	const siteInfo = {
+		collections,
+		tags: [],
+	}
 
-  const createPostPage = (item, data) => {
-    const Template = path.resolve(`src/templates/PostListPage.jsx`);
-    createPage({
-      path: `${item}`,
-      component: Template,
-      // In your blog post template's graphql query, you can use path
-      // as a GraphQL variable to query for data from the markdown file.
-      context: {
-        siteInfo,
-        data
-      },
-    });
-  };
+	const createIndexPage = () => {
+		const Template = path.resolve(`src/templates/IndexPage.jsx`);
+		createPage({
+			path: '/',
+			component: Template,
+			// In your blog post template's graphql query, you can use path
+			// as a GraphQL variable to query for data from the markdown file.
+			context: {
+				siteInfo,
+			},
+		});
+	};
 
-  listPageData.map((item, index) => {
-    createPostPage(collections[index], item);
-  })
+	const listPageData = collections.map(async (item) => {
+		const data = await getOneCollectionList(graphql, item.name);
+		return data;
+	});
 
-  createIndexPage();
+	const createPostPage = (item, data) => {
+		const Template = path.resolve(`src/templates/PostListPage.jsx`);
+		createPage({
+			path: `${item}`,
+			component: Template,
+			// In your blog post template's graphql query, you can use path
+			// as a GraphQL variable to query for data from the markdown file.
+			context: {
+				siteInfo,
+				data
+			},
+		});
+	};
+
+	listPageData.map((item, index) => {
+		createPostPage(collections[index].name, item);
+	})
+
+	createIndexPage();
 
 };
