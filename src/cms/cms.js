42404b080e6e9b2a1426be8fd37967cb7065144e
jounghun.kwon <jounghun.kwon@wadiz.kr>
2020-04-15 15:43:56 +0900

    cms customWidget 수정

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,7 +6,7 @@ import sha1 from 'sha1';
 class CustomWidgetControl extends React.PureComponent {
 	render() {
 		return (
-			<input className="css-83wr9v" type="text" value={moment().format('YYYY-MM-DD HH:mm')} readOnly/>
+			<input className="css-83wr9v" type="text" defaultValue={moment().format('YYYY-MM-DD HH:mm')} readOnly/>
 		);
 	}
 }
@@ -15,8 +15,9 @@ class PostIdWidget extends React.PureComponent {
 	render() {
 		console.log(this.props);
 		const { value } = this.props;
+		const newValue = value || sha1(`postId-${moment().format('YYYY-MM-DD HH:mm')}`);
 		return (
-			<input className="css-83wr9v" type="text" value={value || sha1(`postId-${moment().format('YYYY-MM-DD HH:mm')}`)} />
+			<input className="css-83wr9v" type="text" defaultValue={newValue} />
 		);
 	}
 }
